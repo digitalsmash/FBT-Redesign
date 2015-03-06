72a20aab2606c09854d645c4c60e82a71529a175
digitalsmash <hello@call-me-peter.com>
2015-03-06 15:55:42 +0100

    fbt online mount

---
+++ js/master.js
@@ -0,0 +1,43 @@
+
+
+$(document).on('click', '.mega .dropdown-menu', function(e) {
+  e.stopPropagation()
+})
+
+$(document).ready(function(){
+
+$("#search").focus(function(){$(this).animate({width:"100%"})}).blur(function(){$(this).animate({width:"80px"})});
+
+//
+
+
+$('#myCarousel').carousel({
+interval:   6000
+});
+
+moment.locale('de');
+
+$('#fromDatepicker').datepicker({
+ momentConfig: 
+ {
+    culture: 'de',
+    format: 'L'
+ }
+});
+$('#toDatepicker').datepicker({
+ momentConfig: 
+ {
+    culture: 'de',
+    format: 'L'
+ }
+});
+
+moment.lang('de', {
+    week : {
+        dow : 1 // Monday is the first day of the week
+    }
+})
+//moment.locale('de');
+$('#radioEx1').radio();
+
+});
