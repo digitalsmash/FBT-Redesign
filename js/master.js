b53d565f15892cc393974f94dc624583076ce11e
digitalsmash <hello@call-me-peter.com>
2015-03-12 17:39:30 +0100

    Datepicker

--- js/master.js
+++ js/master.js
@@ -8,33 +8,21 @@ $(document).ready(function(){
 
 $("#search").focus(function(){$(this).animate({width:"100%"})}).blur(function(){$(this).animate({width:"80px"})});
 
+$(function () {
+      $('#datetimepicker1').datetimepicker({
+        locale: 'de',
+        format: 'DD/MM/YYYY'
+      });
+      $('#datetimepicker2').datetimepicker({
+        locale: 'de',
+        format: 'DD/MM/YYYY'
+      });
+    });
 
 $('#myCarousel').carousel({
 interval:   6000
 });
 
-moment.locale('de');
-
-$('#fromDatepicker').datepicker({
- momentConfig: 
- {
-    culture: 'de',
-    format: 'L'
- }
-});
-$('#toDatepicker').datepicker({
- momentConfig: 
- {
-    culture: 'de',
-    format: 'L'
- }
-});
-
-moment.lang('de', {
-    week : {
-        dow : 1 // Monday is the first day of the week
-    }
-})
 //moment.locale('de');
 $('#radioEx1').radio();
 
